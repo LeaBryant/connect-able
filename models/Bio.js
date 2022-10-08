0aa72668f660a7df2d0296a350be09e1ca174258
LeaBryant <ldbryant3220@gmail.com>
2022-10-08 11:07:54 -0700

    added code to Bio.js

--- models/Bio.js
+++ models/Bio.js
@@ -5,16 +5,62 @@ const { Schema, model } = require('mongoose');
 const bioSchema = new Schema(
     {
         lookingforlove: {
-            name: {type: String},
-            required: true
+            type: String,
+            required: true,
+            possibleValues: ['yes', 'no'] 
         },
         lookingforfriends: {
-            name: {type: String},
-            required: true
+            type: String,
+            required: true,
+            possibleValues: ['yes', 'no'] 
         },
         lookingforcommunity: {
-            name: {type: String},
+            type: String,
+            required: true,
+            possibleValues: ['yes', 'no'] 
+        },
+        birthdate: {
+            type: Date,
+            required: true,
+        },
+        gender: {
+            type: String,
+            required: true,
+            possibleValues: ['Woman', 'Man', 'Transgender', 'Non-binary/non-conforming', 'Prefer not to respond'] 
+        },
+        orientation: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        aboutme: {
+            type: String,
+            maxlength: 1000,
+            required: true
+        },
+        photo: {
+            type: String,
+            required: true
+        },
+        interests: {
+            type: String,
+            maxlength: 500,
+            required: true
+        },
+        selfidentify: {
+            type: String,
+            required: true,
+            possibleValues: ['yes', 'no'] 
+        },
+        disability: {
+            type: String,
+            maxlength: 500,
             required: true
         }
     }
 )
+
+module.exports = Bio;
