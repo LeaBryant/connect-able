27092c3cd21a19148e865a2bf5e8ec9bcdbb5282
LeaBryant <ldbryant3220@gmail.com>
2022-10-13 16:54:40 -0700

    added mutations for frontend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,45 @@
 import { gql } from '@apollo/client';
+
+export const NEW_USER = gql`
+  mutation newUser($username: String!, $email: String!, $password: String!) {
+    newUser(name: $name, email: $email, password: $password) {
+      token
+        User {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+        User {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_LOVES = gql`
+  mutation addLoves($userID: ID!) {
+    addLoves(userID: $ID!) {
+      _id
+      name
+      loves
+    }
+  }
+`;
+
+export const ADD_FRIENDS = gql`
+  mutation addFriends($userID: ID!) {
+    addFriends(userID: $ID!) {
+      _id
+      name
+      friends
+    }
+  }
+`;
