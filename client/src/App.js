98e9831338956b235214484f2427447b6287cf74
LeaBryant <ldbryant3220@gmail.com>
2022-10-12 21:43:08 -0700

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
     return(
         <>
-   <h1>Hello World</h1>
+   <h1>Hello Loves</h1>
    </>
     )
 }
