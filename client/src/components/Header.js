6b97a794b2fde35cc84d9ac64af87a0d0e03ea7b
Keli <krsparks2@yahoo.com>
2022-10-15 12:49:37 -0700

    get rid of class and for

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Header = () => {
   return (
-  <div class='container'>
-    <div class='jumbotron'>
-      <div class="text-center">
+  <div className='container'>
+    <div className='jumbotron'>
+      <div className="text-center">
         <img alt='Logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/EasternGraySquirrel_GAm.jpg/1024px-EasternGraySquirrel_GAm.jpg"></img>
       </div>
     </div>
