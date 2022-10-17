0d3517cf998cd72b681c4cb0de00e97f7401e678
LeaBryant <ldbryant3220@gmail.com>
2022-10-17 13:56:05 -0700

    buttons and profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,36 +1,49 @@
 import React from 'react';
 
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
-import '../style.css'
+import { useMutation, useQuery } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+import '../style.css';
+
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+// const myProfile = () => {
+  // const { data } = useQuery(GET_ME);
+  // const userData = data?.me || {};
+// }
 
 
 const Profile = () => {
+  const { loading, data } = useQuery(QUERY_ME);
+  const userData = data?.me || {};
   return (
-  <body>
+  <div className="body">
     <div>
       <Nav />
       </div>
-
-    
-  <div class="container-fluid">
-  <div class="row content">
-    <div class="col-sm-3 sidenav">
-      <h4>John's Blog</h4>
+ 
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+      <Card.Body>
+        <Card.Title>Welcome</Card.Title>
+        <Card.Text>
+          {userData.username}
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item href="#">Messages</ListGroup.Item>
+        <ListGroup.Item href="#">My Connections</ListGroup.Item>
+        <ListGroup.Item href="#">Events</ListGroup.Item>
+      </ListGroup>
       
-      </div>
-    </div>
-
-
-
-
-    </div>
-
+    </Card>
      
     <div>
       <Footer />
     </div>
-  </body>
+  </div>
   );
 };
 
