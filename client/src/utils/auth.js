617c724c8dc6f1b2e97d14fd94c9cb7aa8776515
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-10-16 19:52:41 -0700

    debugging so that client talks to server; we can now create a user

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,17 +1,67 @@
+// // use this to decode a token and get the user's information out of it
+// import decode from 'jwt-decode';
+
+// // create a new class to instantiate for a user
+// class AuthService {
+//   // get user data from JSON web token by decoding it
+//   getUser() {
+//     return decode(this.getToken());
+//   }
+
+//   // return `true` or `false` if token exists (does not verify if it's expired yet)
+//   loggedIn() {
+//     const token = this.getToken();
+//     return token ? true : false;
+//   }
+
+//   getToken() {
+//     // Retrieves the user token from localStorage
+//     return localStorage.getItem('id_token');
+//   }
+
+//   login(idToken) {
+//     // Saves user token to localStorage and reloads the application for logged in status to take effect
+//     localStorage.setItem('id_token', idToken);
+//     window.location.assign('/');
+//   }
+
+//   logout() {
+//     // Clear user token and profile data from localStorage
+//     localStorage.removeItem('id_token');
+//     // this will reload the page and reset the state of the application
+//     window.location.reload();
+//   }
+// }
+
+// export default new AuthService();
+
 // use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
 
 // create a new class to instantiate for a user
 class AuthService {
-  // get user data from JSON web token by decoding it
-  getUser() {
+  // get user data
+  getProfile() {
     return decode(this.getToken());
   }
 
-  // return `true` or `false` if token exists (does not verify if it's expired yet)
+  // check if user's logged in
   loggedIn() {
+    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return token ? true : false;
+    return !!token && !this.isTokenExpired(token); // handwaiving here
+  }
+
+  // check if token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
   }
 
   getToken() {
@@ -20,16 +70,16 @@ class AuthService {
   }
 
   login(idToken) {
-    // Saves user token to localStorage and reloads the application for logged in status to take effect
+    // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
+    window.location.assign('/Profile');
   }
 
   logout() {
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
     // this will reload the page and reset the state of the application
-    window.location.reload();
+    window.location.assign('/');
   }
 }
 
