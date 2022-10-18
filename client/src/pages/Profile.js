0a10e7283350ba690e17f4b8a6877b95c5233ba3
LeaBryant <ldbryant3220@gmail.com>
2022-10-17 17:25:27 -0700

    added screen shots to readme

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -27,7 +27,7 @@ const Profile = () => {
   <Container>
     <Row>
   <Col sm={3}>
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '15rem' }}>
       <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
       <Card.Body>
         <Card.Title>Welcome</Card.Title>
@@ -43,39 +43,39 @@ const Profile = () => {
     </Card>
     </Col>
 
-    <Col sm={9}>
-    <div class="d-flex justify-content-center row">
-        <div class="col-md-12">
-            <div class="feed p-2">
-                <div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white border">
-                    <div class="feed-text px-2">
-                        <h6 class="text-black-50 mt-2">What's on your mind</h6>
+    <Col sm={8}>
+    <div className="d-flex justify-content-center row">
+        <div className="col-md-15">
+            <div className="feed p-1">
+                <div className="d-flex flex-row justify-content-between align-items-center p-2 bg-white border">
+                    <div className="feed-text px-3">
+                        <h6 className="text-black-50 mt-2">What's on your mind</h6>
                     </div>
-                    <div class="feed-icon px-2"><i class="fa fa-long-arrow-up text-black-50"></i></div>
+                    <div className="feed-icon px-2"><i className="fa fa-long-arrow-up text-black-50"></i></div>
                 </div>
-                <div class="bg-white border mt-2">
+                <div className="bg-white border mt-2">
                     <div>
-                        <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-                            <div class="d-flex flex-row align-items-center feed-text px-2"><img class="rounded-circle" src="https://i.imgur.com/aoKusnD.jpg" width="45"/>
-                                <div class="d-flex flex-column flex-wrap ml-2"><span class="font-weight-bold">Thomson ben</span><span class="text-black-50 time">40 minutes ago</span></div>
+                        <div className="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
+                            <div className="d-flex flex-row align-items-center feed-text px-2"><img className="rounded-circle" src="https://i.imgur.com/aoKusnD.jpg" width="45"/>
+                                <div className="d-flex flex-column flex-wrap ml-2"><span className="font-weight-bold">Thomson ben</span><span className="text-black-50 time">40 minutes ago</span></div>
                             </div>
-                            <div class="feed-icon px-2"><i class="fa fa-ellipsis-v text-black-50"></i></div>
+                            <div className="feed-icon px-2"><i className="fa fa-ellipsis-v text-black-50"></i></div>
                         </div>
                     </div>
-                    <div class="p-2 px-3"><span>Whoa crazy night last night!!😎 </span></div>
-                    <div class="d-flex justify-content-end socials p-2 py-3"><i class="fa fa-thumbs-up"></i><i class="fa fa-comments-o"></i><i class="fa fa-share"></i></div>
+                    <div className="p-2 px-3"><span>Whoa crazy night last night!!😎 </span></div>
+                    <div className="d-flex justify-content-end socials p-2 py-3"><i className="fa fa-thumbs-up"></i><i className="fa fa-comments-o"></i><i className="fa fa-share"></i></div>
                 </div>
-                <div class="bg-white border mt-2">
+                <div className="bg-white border mt-2">
                     <div>
-                        <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-                            <div class="d-flex flex-row align-items-center feed-text px-2"><img class="rounded-circle" src="https://i.imgur.com/aoKusnD.jpg" width="45"/>
-                                <div class="d-flex flex-column flex-wrap ml-2"><span class="font-weight-bold">Thomson ben</span><span class="text-black-50 time">40 minutes ago</span></div>
+                        <div className="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
+                            <div className="d-flex flex-row align-items-center feed-text px-2"><img className="rounded-circle" src="https://i.imgur.com/aoKusnD.jpg" width="45"/>
+                                <div className="d-flex flex-column flex-wrap ml-2"><span className="font-weight-bold">Thomson ben</span><span className="text-black-50 time">40 minutes ago</span></div>
                             </div>
-                            <div class="feed-icon px-2"><i class="fa fa-ellipsis-v text-black-50"></i></div>
+                            <div className="feed-icon px-2"><i className="fa fa-ellipsis-v text-black-50"></i></div>
                         </div>
                     </div>
-                    <div class="feed-image p-2 px-3"><img class="img-fluid img-responsive" src="http://eyesimple.us/site/wp-content/uploads/2018/01/Call-Tech-Support-My-Mouse-Is-Dead-Funny-Technology-Meme-Image.jpg"/></div>
-                    <div class="d-flex justify-content-end socials p-2 py-3"><i class="fa fa-thumbs-up"></i><i class="fa fa-comments-o"></i><i class="fa fa-share"></i></div>
+                    <div className="feed-image p-2 px-3"><img className="img-fluid img-responsive" src="http://eyesimple.us/site/wp-content/uploads/2018/01/Call-Tech-Support-My-Mouse-Is-Dead-Funny-Technology-Meme-Image.jpg"/></div>
+                    <div className="d-flex justify-content-end socials p-2 py-3"><i className="fa fa-thumbs-up"></i><i className="fa fa-comments-o"></i><i className="fa fa-share"></i></div>
                 </div>
             </div>
     </div>
